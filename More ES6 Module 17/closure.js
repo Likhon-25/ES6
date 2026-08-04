a9e579d0a364519e27bd111a4fa3d541dab2c3d4
Likhon-25 <likhonmondol327@gmail.com>
2026-08-04 09:57:47 +0600

    learn JS closure

---
+++ More ES6 Module 17/closure.js
@@ -0,0 +1,15 @@
+function createCounter(){
+    let counter = 0;
+
+    return function(){
+        counter++;
+        return counter;
+    }
+
+}
+let counter = createCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
