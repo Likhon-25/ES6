585882117dbe7d6125edc136309b2047e83932b1
Likhon-25 <likhonmondol327@gmail.com>
2026-08-07 16:22:36 +0600

    learn forEach

---
+++ More ES6 Module 17/for_each.js
@@ -0,0 +1,11 @@
+let rools = [10,12,24,30];
+rools.forEach((elem, ind, arr) =>{
+    // console.log(elem, ind, arr);
+})
+
+let numbers = [10, 20, 30];
+
+numbers.forEach(function (num) {
+  console.log(num * 2);
+});
+
